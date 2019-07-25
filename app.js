d134347b1de6a9a0618668cd6a674b19b582acce
huangboju <bhuang@xiaohongshu.com>
2019-07-25 20:31:32 +0800

    Update app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var j = 10;
+var i = 10;
+var m = i + j;
+console.info("hellow world");
+
+if (true) {
+  var x = 5;
+}
+console.log(x); // 5
+
+if (true) {
+  let y = 5;
+}
+console.log(y); // ReferenceError: y 没有被声明
