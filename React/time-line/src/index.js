762b00eafa995d8f7b8a765bcf55a4ad54d091ee
huangboju <bhuang@xiaohongshu.com>
2019-09-20 15:54:45 +0800

    加入timeline

---
+++ React/time-line/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import Timeline from './components/Timeline/Timeline';
+
+
+ReactDOM.render(<Timeline />, document.getElementById('root'));
