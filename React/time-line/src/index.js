d9b82ba7a6694ec8aff1e79ce20ef074c448c4c8
huangboju <bhuang@xiaohongshu.com>
2019-09-25 17:14:14 +0800

    加入Clock

--- React/time-line/src/index.js
+++ React/time-line/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
+import Clock from './components/Timer/Clock';
 import Timeline from './components/Timeline/Timeline';
 
 
 ReactDOM.render(<Timeline />, document.getElementById('root'));
+
+ReactDOM.render(<Clock />, document.getElementById('clock'));
