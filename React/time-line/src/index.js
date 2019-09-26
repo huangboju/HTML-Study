43228b54010441fe82d6944b4d17d4deac9d9a4a
huangboju <bhuang@xiaohongshu.com>
2019-09-26 20:36:28 +0800

    加入代码

--- React/time-line/src/index.js
+++ React/time-line/src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import Clock from './components/Timer/Clock';
+import FetchedTimeline from './FetchedTimeline';
 import Timeline from './components/Timeline/Timeline';
+import Container from './Container';
 
 
 ReactDOM.render(<Timeline />, document.getElementById('root'));
+
+ReactDOM.render(<FetchedTimeline />, document.getElementById('fetchedTimeline'));
+
+ReactDOM.render(<Container />, document.getElementById('requestRefresh'));
 
 ReactDOM.render(<Clock />, document.getElementById('clock'));
