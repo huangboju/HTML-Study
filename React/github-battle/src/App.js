c0fb01098cfaa4ea8771ee63a9ab1501a2d9a2b9
huangboju <bhuang@xiaohongshu.com>
2019-11-24 15:54:50 +0800

    format code

--- React/github-battle/src/App.js
+++ React/github-battle/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
-import NavBar from './components/nav-bar/nav-bar.component';
-import { ThemeProvider } from './contexts/theme';
+import React from "react";
+import "./App.css";
+import NavBar from "./components/nav-bar/nav-bar.component";
+import { ThemeProvider } from "./contexts/theme";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Loading from './components/loading/loading.component';
+import Loading from "./components/loading/loading.component";
 
 const Popular = React.lazy(() => import("./pages/popular/popular.component"));
 const Battle = React.lazy(() => import("./pages/battle/battle.component"));
@@ -12,7 +12,7 @@ class App extends React.Component {
   state = {
     theme: "light",
     toggleTheme: () => {
-      this.setState(({theme}) => ({
+      this.setState(({ theme }) => ({
         theme: theme === "light" ? "dark" : "light"
       }));
     }
--- React/github-battle/src/components/nav-bar/nav-bar.component.jsx
+++ React/github-battle/src/components/nav-bar/nav-bar.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { ThemeConsumer } from "../../contexts/theme";
 import { FaLightbulb, FaRegLightbulb } from "react-icons/fa";
@@ -6,38 +6,37 @@ import { FaLightbulb, FaRegLightbulb } from "react-icons/fa";
 import "./nav-bar.style.scss";
 
 const activeStyle = {
-    color: "rgb(187, 46, 31)"
+  color: "rgb(187, 46, 31)"
 };
 
 const NavBar = () => {
-    return (
-        <ThemeConsumer>
-            {({theme, toggleTheme}) => (
-                <nav className="nav-bar">
-                    <ul>
-                       <li>
-                           <NavLink exact to="/" activeStyle={activeStyle}>
-                               Popular
-                           </NavLink>
-                       </li> 
-                       <li>
-                           <NavLink exact to="/battle" activeStyle={activeStyle}>
-                               Battle
-                           </NavLink>
-                       </li> 
-                    </ul>
-                       <button onClick={toggleTheme}>
-                           {theme === "light" ? (
-                               <FaLightbulb size="40" color="#1C2022" />
-                           ) : (
-                               <FaRegLightbulb size="40" color="#FFD679" />
-                           )
-                        }
-                       </button>
-                </nav>
+  return (
+    <ThemeConsumer>
+      {({ theme, toggleTheme }) => (
+        <nav className="nav-bar">
+          <ul>
+            <li>
+              <NavLink exact to="/" activeStyle={activeStyle}>
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact to="/battle" activeStyle={activeStyle}>
+                Battle
+              </NavLink>
+            </li>
+          </ul>
+          <button onClick={toggleTheme}>
+            {theme === "light" ? (
+              <FaLightbulb size="40" color="#1C2022" />
+            ) : (
+              <FaRegLightbulb size="40" color="#FFD679" />
             )}
-        </ThemeConsumer>
-   );
+          </button>
+        </nav>
+      )}
+    </ThemeConsumer>
+  );
 };
 
 export default NavBar;
