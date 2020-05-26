2532cb719831e32bc90f80919e85e46eaf7f29f9
huangboju <bhuang@xiaohongshu.com>
2020-05-26 17:38:18 +0800

    完成MyGame

--- React/tic-tac-toe/src/index.js
+++ React/tic-tac-toe/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
+import MyGame from './MyGame';
 import Temperature from './Temperature';
 
 // ========================================
 
 ReactDOM.render(
   // <Game />,
-  <Temperature />,
+  <MyGame />,
+  // <Temperature />,
   document.getElementById('root')
 );
